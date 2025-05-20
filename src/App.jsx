0c7577351d43b22667bb43d5931b7306f55dad7a
Shreeraj Shrestha <shreerajshrestha.dev@gmail.com>
2025-05-20 08:22:44 +0545

    Completed Basic Setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import {} from 'react'
+
+function App() {
+  return (
+    <div className = "w-full h-screen bg-gradient-to-r from-slate-950 to-gray-900 text-gray-100 font-custom flex flex-col items-center">
+      <h1 className = "text-5xl">Book Search</h1>
+    </div>
+  )
+}
+
+export default App
